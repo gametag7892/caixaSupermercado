b1a9bb59325dc9c90873193f7798b88b05b74a97
gametag7892 <78163768+gametag7892@users.noreply.github.com>
2025-11-12 13:32:03 -0300

    Fix constructor formatting in ProdutoContext

--- Model.cs
+++ Model.cs
@@ -8,7 +8,8 @@ public class Produto:DBContext
   public DbSet<Produto> Produto {get; set;}
   public string DbPath {get;}
 
-  public ProdutoContext(){
+  public ProdutoContext()
+  {
     DbPath = "sistema.db";
   }
   
